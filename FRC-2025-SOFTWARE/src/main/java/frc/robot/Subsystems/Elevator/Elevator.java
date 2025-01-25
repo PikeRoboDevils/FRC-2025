51237e4015a42c2add36eab02377bdedf4e0178b
Jackson Lee <jacksonlee050802@gmail.com>
2025-01-25 16:03:21 -0500

    add subsystems, hadrware, and IO

---
+++ FRC-2025-SOFTWARE/src/main/java/frc/robot/Subsystems/Elevator/Elevator.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.Elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase {
+  /** Creates a new Elevator. */
+  ElevatorIO io;
+  public Elevator(ElevatorIO elevatorIO) {
+    this.io = elevatorIO;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
