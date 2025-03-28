e0638f6097f110af11c99978966436b54a466ff1
PikeRoboDevils1018 <161903990+PikeRoboDevils1018@users.noreply.github.com>
2025-03-28 08:32:07 -0400

    Update at 'Fri Mar 28 08:32:07 EDT 2025'

--- FRC-2025-SOFTWARE/src/main/java/frc/robot/Subsystems/Wrist/Wrist.java
+++ FRC-2025-SOFTWARE/src/main/java/frc/robot/Subsystems/Wrist/Wrist.java
@@ -27,50 +27,51 @@ public class Wrist extends SubsystemBase {
 
   private boolean isBad;
 
-  public BooleanSupplier wristDisabled = ()->isBad;
+  public BooleanSupplier wristDisabled = () -> isBad;
 
   /** Creates a new Wrist. */
   public Wrist(WristIO wristIo, Elevator elevatorStage) {
     this.io = wristIo;
     this.elevatorStage = elevatorStage;
 
-    _wristPose =
-        new Pose3d(
-            new Translation3d(0, 0, 0).plus(elevatorStage.stage3Visuals.getTranslation()),
-            new Rotation3d(0, 0, 0));
+    _wristPose = new Pose3d(
+        new Translation3d(0, 0, 0).plus(elevatorStage.stage3Visuals.getTranslation()),
+        new Rotation3d(0, 0, 0));
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    _wristPose =
-        new Pose3d(
-            new Translation3d(0.065, 0.0, 0.095).plus(elevatorStage.stage3Visuals.getTranslation()),
-            new Rotation3d(
-                0, Units.degreesToRadians(io.getAngleDeg() + 90), Units.degreesToRadians(180)));
+    _wristPose = new Pose3d(
+        new Translation3d(0.065, 0.0, 0.095).plus(elevatorStage.stage3Visuals.getTranslation()),
+        new Rotation3d(
+            0, Units.degreesToRadians(io.getAngleDeg() + 90), Units.degreesToRadians(180)));
 
     Logger.recordOutput("Components/Wrist", _wristPose);
-
 
     Logger.recordOutput("WRIST BAD", wristDisabled.getAsBoolean());
 
     io.updateInputs(inputs);
     Logger.processInputs("wrist", inputs);
   }
-  // so we can set command to null 
+
+  // so we can set command to null
   public Command setAngle(DoubleSupplier angle) {
-      if (wristDisabled.getAsBoolean()) { return Commands.none();}// to prevent movement
+    if (wristDisabled.getAsBoolean()) {
+      return Commands.none();
+    } else {// to prevent movement
       return run(() -> io.setAngle(angle.getAsDouble()));
+    }
 
   }
 
   public Command toggle() {
-    
-    return Commands.runOnce(()->isBad = !isBad,this);
+
+    return Commands.runOnce(() -> isBad = !isBad, this);
   }
 
-  public boolean getIsBad(){
-      return wristDisabled.getAsBoolean();
+  public boolean getIsBad() {
+    return wristDisabled.getAsBoolean();
   }
 
   public Command home() {
