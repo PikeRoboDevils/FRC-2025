dcd9a315cf8ab6f3b8736ef4094c9a9f72bd573c
PikeRoboDevils1018 <161903990+PikeRoboDevils1018@users.noreply.github.com>
2025-04-04 10:08:42 -0400

    Update at 'Fri Apr 04 10:08:42 EDT 2025'

--- FRC-2025-SOFTWARE/src/main/java/frc/robot/Subsystems/Sweve/Swerve.java
+++ FRC-2025-SOFTWARE/src/main/java/frc/robot/Subsystems/Sweve/Swerve.java
@@ -88,7 +88,7 @@ public class Swerve extends SubsystemBase {
     this.io = swerveIO;
     Constants.Swerve.targetPosition[17][0] = new Pose2d(new Translation2d(0, 0), new Rotation2d(0));
 
-    // CameraServer.startAutomaticCapture("climb", 0).setResolution(200, 400);
+    CameraServer.startAutomaticCapture("climb", 0).setResolution(200, 400);
 
     // CameraServer.startAutomaticCapture("elev", 1).setResolution(200, 400);
 
